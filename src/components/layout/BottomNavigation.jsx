c164292cb6394c3e620a6a7859212af6876c5557
Lizst777 <2151459508@qq.com>
2026-08-18 15:49:57 +0800

    Preserve liquid dock filtering in production

--- src/components/layout/BottomNavigation.jsx
+++ src/components/layout/BottomNavigation.jsx
@@ -1,6 +1,13 @@
 import { NAV_ITEMS } from "../../constants/navigation";
 import { NavIcon } from "../../ui/Icons";
 
+const LIQUID_DOCK_FILTER =
+  "blur(30px) saturate(190%) contrast(105%)";
+const LIQUID_DOCK_STYLE = {
+  backdropFilter: LIQUID_DOCK_FILTER,
+  WebkitBackdropFilter: LIQUID_DOCK_FILTER,
+};
+
 function BottomNavigation({ page, onPageChange }) {
   const destination = NAV_ITEMS.find((item) => item.id !== page) ?? NAV_ITEMS[0];
 
@@ -9,7 +16,7 @@ function BottomNavigation({ page, onPageChange }) {
       className={`bottom-nav bottom-nav--${page}`}
       aria-label="Workspace navigation"
     >
-      <div className="bottom-nav__container">
+      <div className="bottom-nav__container" style={LIQUID_DOCK_STYLE}>
         <button
           type="button"
           className="bottom-nav__item"
